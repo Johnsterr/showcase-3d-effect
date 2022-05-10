9c22bc395ee3714761594ba3cfc6b62dac3847bf
Evgeniy Pashko <evgpashko92@gmail.com>
2022-05-11 00:44:18 +0700

    Init swiper-slider

--- js/common.js
+++ js/common.js
@@ -1,3 +1,9 @@
 document.addEventListener("DOMContentLoaded", function () {
+  const showSlider = new Swiper(".showcase-carousel", {
+    loop: true,
+    slidesPerView: 3,
+    speed: 1800,
+  });
+
   document.querySelector("video").playbackRate = 2;
 });
