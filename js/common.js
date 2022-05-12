a5a91cd5c403c161a31baa99c4ee6b311a7a6b6e
Evgeniy Pashko <evgpashko92@gmail.com>
2022-05-12 22:58:16 +0700

    Add actual slides, create transitions between slides, custom cubic-bezier transition effect

--- js/common.js
+++ js/common.js
@@ -3,6 +3,7 @@ document.addEventListener("DOMContentLoaded", function () {
     loop: true,
     slidesPerView: 3,
     speed: 1800,
+    centeredSlides: true,
   });
 
   document.querySelector("video").playbackRate = 2;
